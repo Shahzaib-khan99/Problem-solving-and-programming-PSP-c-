c2ef82ab1f8df85ec51f6a747704efe430678b8b
Shahzaib Khan <aikingshahzaib0011277711@gmail.com>
2026-08-03 21:59:30 +0500

    Add files via upload

---
+++ character program 12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter a character";
+    cin>>ch;
+    if( ch=='u')
+    {
+    cout<<"i feel great today!";
+    }
+    else
+    {
+    	cout<<"i feel down today #$*!";
+    }
+    }
